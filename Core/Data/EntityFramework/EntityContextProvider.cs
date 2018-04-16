1def36eb69b00012c2cf712d33b61bbfa9e82588
Cody <luckylohse@gmail.com>
2018-04-15 23:23:02 -0500

    Removing unused code

--- Core/Data/EntityFramework/EntityContextProvider.cs
+++ Core/Data/EntityFramework/EntityContextProvider.cs
@@ -1,12 +1,9 @@
 using System;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using Core;
-using Data.EntityFramework.Extensions;
-using Microsoft.AspNetCore.Hosting;
 using Core.Data.EntityFramework;
 
 namespace Data.EntityFramework
@@ -15,15 +12,9 @@ namespace Data.EntityFramework
     {
         private DbContext Context {get; set;}
 
-        private IConfiguration Configuration;
-
-        private readonly IHostingEnvironment HostingEnvironment;
-
         private IDbContextFactory<TContext> DbContextFactory;
 
-        public EntityContextProvider(IConfiguration configuration, IHostingEnvironment hostingEnvironment, IDbContextFactory<TContext> dbContextFactory){
-            this.Configuration = configuration;
-            this.HostingEnvironment = hostingEnvironment;
+        public EntityContextProvider(IDbContextFactory<TContext> dbContextFactory){
             this.DbContextFactory = dbContextFactory;
         }
 
@@ -34,20 +25,6 @@ namespace Data.EntityFramework
                 if (this.Context == null)
                 {
                     this.Context = this.DbContextFactory.CreateDbContext();
-                    
-                    //var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-                    //optionsBuilder.GetDbProvider(this.HostingEnvironment.EnvironmentName, this.Configuration["ConnectionStrings:DefaultConnection"]);
-
-                    //this.Context = new ApplicationDbContext(optionsBuilder.Options);
-
-                    //// TODO this should be moved to the testing class instead of here
-                    //// TODO not sure right now if it runs the latest migration?
-                    //if (this.HostingEnvironment.EnvironmentName.Equals("IntegrationTesting"))
-                    //{
-                    //    // Delete the database per test. This wil probably cause horrible integration testing latency if many tests are to be run
-                    //    this.Context.Database.EnsureDeleted();
-                    //    this.Context.Database.Migrate();
-                    //}
                 }
 
                 return this.Context;
